8f9efcea7093bd9b053d96d10bfb8f7bc6ba4f2b
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-22 15:13:03 -0300

    Using Express Router

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use(bodyParser.json({limit: 100 * 1024 * 1024}));
-// app.use(bodyParser.urlencoded({limit: 100 * 1024 * 1024}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
