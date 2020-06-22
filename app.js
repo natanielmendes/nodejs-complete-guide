649ccb9ee569147f07fbe50cd287d6aa4a215465
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-22 15:28:57 -0300

    Filtering Paths

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
