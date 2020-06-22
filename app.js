1908a4bbec50690b53caa768a3ae4b1ba3318aff
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-22 15:21:30 -0300

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
