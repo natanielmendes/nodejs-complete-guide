997427ecae7d5eaaa6691992b013d85550ad321c
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-24 21:08:24 -0300

    Converting HTML Files to Pug

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product'})
 });
 
 // /admin/add-product => POST
