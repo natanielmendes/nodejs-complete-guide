018f530bae2186d310c568c9cc8b5a7ff2fac0c9
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-27 17:42:44 -0300

    Finishing the controllers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found'});
-});
+app.use(errorController.getNotFound);
 
 app.listen(3000);
